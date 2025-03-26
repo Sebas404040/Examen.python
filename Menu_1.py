605206c5672b3100b184831cc867205613315cec
Sebas404040 <delgadopalitogarbage2gmail.com>
2025-03-26 07:19:43 -0500

    feat: Se agrega un menu general del programa

---
+++ Menu_1.py
@@ -0,0 +1,31 @@
+def menu_principal():
+    from Funcionalidad import registrar_artista
+    print("Bienvenido")
+    print("""
+    1. Registrar artistas
+    2. Registrar genero musical
+    3. Registrar pais
+    4. Informes
+    5. Salir
+    """)
+    try:
+        while True:
+            print("Ingrese una opcion: ")
+            decision_main=int(input("Opcion: "))
+            if decision_main>5 or decision_main<1:
+                print("Opcion incorrecta, no existe esa opcion")
+            elif decision_main==1:
+                print("Registrar artistas")
+                registrar_artista()
+            elif decision_main==2:
+                print ("Registrar genero musical")
+            elif decision_main==3:
+                print ("Registrar pais")
+            elif decision_main==4:
+                print ("Informes")
+            elif decision_main==5:
+                print("Saliendo...")
+                break
+    except (ValueError):
+        print("Ingreso un valor no valido")
+menu_principal()
